b4543fd91dd92b32fffccc1432c57c773c2fdb0d
Muhannad Al-Khatib <mmalkhatib@hotmail.com>
2021-02-01 16:32:00 +0200

    defaulting heartbeat to be disabled

--- SW.Bus/BusOptions.cs
+++ SW.Bus/BusOptions.cs
@@ -14,11 +14,15 @@ namespace SW.Bus
             DefaultQueuePrefetch = 4;
             DefaultRetryCount = 5;
             DefaultRetryAfter = 60;
+            HeartBeatTimeOut = 0;
             Token = new JwtTokenParameters();
             ProcessExchange = $"{versionPrefix}{environment}".ToLower();
             DeadLetterExchange = $"{versionPrefix}{environment}.deadletter".ToLower();
         }
+        public int HeartBeatTimeOut { get; set; }
 
+        internal TimeSpan RequestedHeartbeat =>
+            HeartBeatTimeOut > 0 ? TimeSpan.FromSeconds(HeartBeatTimeOut) : TimeSpan.Zero; 
         public JwtTokenParameters Token { get; set; }
         public string ApplicationName { get; set; }
         public ushort DefaultQueuePrefetch { get; set; }
@@ -27,6 +31,7 @@ namespace SW.Bus
         public IDictionary<string, QueueOptions> Options { get; }
         public string ProcessExchange { get; }
         public string DeadLetterExchange { get; }
+        
         public void AddQueueOption(string queueName, ushort? prefetch = null, int? retryCount = null, uint? retryAfterSeconds = null)
         {
             Options[queueName.ToLower()] = new QueueOptions
